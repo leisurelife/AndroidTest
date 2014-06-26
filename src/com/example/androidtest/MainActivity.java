6dc270cf54c462bf6421b35df4ea541a2d2ca4c2
leisurelife <leisurelife1990@gmail.com>
2014-06-26 13:49:52 +0800

    添加dialog响应事件

--- src/com/example/androidtest/MainActivity.java
+++ src/com/example/androidtest/MainActivity.java
@@ -1,18 +1,43 @@
 package com.example.androidtest;
 
+import com.example.androidtest.MyDlog.MulClick;
+
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
 	MyDlog mDialog;
+	Button mShowBT;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		mShowBT=(Button) findViewById(R.id.show);
 		mDialog=new MyDlog(this);
+		mDialog.setMulClick(new MulClick() {
+			
+			@Override
+			public void toast() {
+				// TODO Auto-generated method stub
+				if(!mShowBT.getText().toString().equals("我变成这样了。。。")){
+					mShowBT.setText("我变成这样了。。。");
+				}else{
+					mShowBT.setText(R.string.hello_world);
+				}
+				
+			}
+			
+			@Override
+			public void cancle() {
+				// TODO Auto-generated method stub
+				mDialog.cancel();
+			}
+		});
 	}
 
 	
@@ -20,4 +45,5 @@ public class MainActivity extends Activity {
 		mDialog.show();
 	}
 
+	
 }
--- src/com/example/androidtest/MyDlog.java
+++ src/com/example/androidtest/MyDlog.java
@@ -5,16 +5,33 @@ import android.content.Context;
 import android.graphics.Path.FillType;
 import android.graphics.drawable.BitmapDrawable;
 import android.view.Gravity;
+import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
-public class MyDlog extends Dialog{
+public class MyDlog extends Dialog implements android.view.View.OnClickListener{
+	
+	public interface MulClick{
+		public abstract void cancle();
+		
+		public abstract void toast();
+		
+	}
 
+	private Button mCancleBT;
+	
+	private Button mToastBT;
+	
+	private MulClick mMulClick;
 	public MyDlog(Context context, int theme) {
 		super(context, theme);
 		// TODO Auto-generated constructor stub
 		setCancelable(true);
 		setContentView(R.layout.dialog);
+		mCancleBT=(Button) findViewById(R.id.cancle);
+		mToastBT=(Button)findViewById(R.id.toast);
+		mCancleBT.setOnClickListener(this);
+		mToastBT.setOnClickListener(this);
 		getWindow().setLayout(ViewGroup.LayoutParams.FILL_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
 		getWindow().setGravity(Gravity.BOTTOM);
 		getWindow().setWindowAnimations(R.style.dialog_bottom_enterandout);
@@ -24,5 +41,26 @@ public class MyDlog extends Dialog{
 		this(context,R.style.dialog_style);
 		
 	}
+
+	public void setMulClick(MulClick m){
+		this.mMulClick=m;
+	}
+	
+	@Override
+	public void onClick(View arg0) {
+		// TODO Auto-generated method stub
+		switch (arg0.getId()) {
+		case R.id.cancle:
+			mMulClick.cancle();
+			break;
+		case R.id.toast:
+			mMulClick.toast();
+			break;
+		default:
+			break;
+		}
+	}
+	
+	
 
 }
